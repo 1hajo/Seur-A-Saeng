02144c9296772e9a745be4e64fb57f3c14309fd3
leejeongjun <backbone94@naver.com>
2025-06-10 12:36:41 +0900

    fix: API 타임아웃 시간 임시로 20000ms로 수정

--- seurasaeng_fe/src/libs/axios.ts
+++ seurasaeng_fe/src/libs/axios.ts
@@ -3,7 +3,7 @@ import { API_BASE_URL } from "../constants/env";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  timeout: 20000,
   headers: {
     "Content-Type": "application/json",
   },
