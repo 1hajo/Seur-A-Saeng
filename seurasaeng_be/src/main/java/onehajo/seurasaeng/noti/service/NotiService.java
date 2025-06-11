8249d2d62ad6eb0f1b9927af58646b0866cc5c95
youjiyeon <jiyeonyou0416@naver.com>
2025-06-11 14:51:23 +0900

    feat: newnoti 기능 추가

--- seurasaeng_be/src/main/java/onehajo/seurasaeng/noti/service/NotiService.java
+++ seurasaeng_be/src/main/java/onehajo/seurasaeng/noti/service/NotiService.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import onehajo.seurasaeng.entity.Manager;
 import onehajo.seurasaeng.entity.Noti;
 import onehajo.seurasaeng.inquiry.repository.ManagerRepository;
-import onehajo.seurasaeng.newnoti.repository.NewnotiRepository;
 import onehajo.seurasaeng.noti.dto.NotiReqDTO;
 import onehajo.seurasaeng.noti.dto.NotiResDTO;
 import onehajo.seurasaeng.noti.repository.NotiRepository;
