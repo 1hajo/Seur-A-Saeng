326cf1103c1e5b446a2f3af2450e81b25eb84f73
lee1684 <78637439+backbone94@users.noreply.github.com>
2025-06-11 16:49:07 +0900

    Delete seurasaeng_be directory

--- seurasaeng_be/src/main/java/onehajo/seurasaeng/inquiry/exception/UserException.java
+++
@@ -1,7 +0,0 @@
-package onehajo.seurasaeng.inquiry.exception;
-
-public class UserException extends RuntimeException {
-    public UserException(String message) {
-        super(message);
-    }
-}
--- seurasaeng_be/src/main/java/onehajo/seurasaeng/qr/exception/ShuttleNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package onehajo.seurasaeng.qr.exception;
-
-public class ShuttleNotFoundException extends RuntimeException {
-    public ShuttleNotFoundException(String message) { super(message); }
-}
